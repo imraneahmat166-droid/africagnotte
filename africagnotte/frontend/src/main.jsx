255323338f611bd74d55ad8b9d94034890d46566
imraneahmat166-droid <imraneahmat166@gmail.com>
2026-06-11 01:20:48 +0200

    Update main.jsx

--- africagnotte/frontend/src/main.jsx
+++ africagnotte/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AuthProvider } from './lib/auth'
 
 // Styles globaux
 const style = document.createElement('style')
@@ -15,6 +16,8 @@ document.head.appendChild(style)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 )
